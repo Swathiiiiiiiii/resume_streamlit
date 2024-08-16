7bd3cc55a113bfd25055d1532655a20671c24b4b
swathi gowda <swathigowda1035@gmail.com>
2024-08-16 22:35:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,131 @@
+import streamlit as st
+from PIL import Image
+
+with open("style.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+#####################
+# Header 
+st.write('''
+# A N SWATHI
+##### *Resume* 
+''')
+
+image = Image.open('dp.jpg')
+st.image(image, width=150)
+
+st.markdown('## bio', unsafe_allow_html=True)
+st.info('''
+- student at siddaganga institute of technology -computer science and engineering.
+- currenty focusing on coding and development
+
+''')
+
+#####################
+# Navigation
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">SWATHI GOWDA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#education">Education</a>
+      </li>
+     
+      
+      
+    
+  </div>
+</nav>
+""", unsafe_allow_html=True)
+
+#####################
+# Custom function for printing text
+def txt(a, b):
+  col1, col2 = st.columns([4,1])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+
+def txt2(a, b):
+  col1, col2 = st.columns([1,4])
+  with col1:
+    st.markdown(f'`{a}`')
+  with col2:
+    st.markdown(b)
+
+def txt3(a, b):
+  col1, col2 = st.columns([1,2])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+  
+def txt4(a, b, c):
+  col1, col2, col3 = st.columns([1.5,2,2])
+  with col1:
+    st.markdown(f'`{a}`')
+  with col2:
+    st.markdown(b)
+  with col3:
+    st.markdown(c)
+
+#####################
+st.markdown('''
+## Education
+''')
+
+txt('**jawahara navodaya vidyalaya** (middle+high school and pre university), *mavinkere*, hassan',
+'2015-2022')
+st.markdown('''
+- percentage: 10th-92%
+               12th-93%
+            
+            
+
+''')
+txt('**siddaganga institute of technology** (bachelor of engineering), *tumkur karnataka*',
+'2022-2026')
+st.markdown('''
+
+            
+            
+
+''')
+
+
+
+
+
+
+
+
+
+#####################
+st.markdown('''
+## Skills
+''')
+txt3('Programming', '`Python`, `java`, `c`')
+txt3('Data processing/wrangling', ' `pandas`, `numpy`')
+txt3('Data visualization', ' `seaborn`, `plotly`, `altair`')
+txt3('Machine Learning', '`scikit-learn`')
+txt3('Web development', '`Flask`, `HTML`, `CSS`')
+txt3('Model deployment', '`streamlit`')
+
+#####################
+st.markdown('''
+## Social Media
+''')
+txt2('LinkedIn', 'https://www.linkedin.com/in/swathi-gowda-7713b71bb')
+txt2('Twitter', 'https://x.com/SwathiG65832064?t=Fq6FrDpz_75BvSJNzCuj3g&s=08')
+txt2('GitHub', 'https://github.com/Swathiiiiiiiii')
